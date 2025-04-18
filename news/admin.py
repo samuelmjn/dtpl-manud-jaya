dd9e70fdbda7e8576b0e84d3565b77b0e83e12ee
Irfan <muhammad.irfan413@ui.ac.id>
2025-04-18 11:09:47 +0700

    hotfix: disable slug edit

--- news/admin.py
+++ news/admin.py
@@ -8,9 +8,8 @@ class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'date', 'author')
     list_filter = ('category', 'date', 'author')
     search_fields = ('title', 'content', 'category', 'author')
-    prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'date'
     ordering = ('-date',)
     
     # Membuat field readonly setelah pembuatan
-    readonly_fields = ('date',)
+    readonly_fields = ('date', 'slug')
